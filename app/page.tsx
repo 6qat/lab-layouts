baf4d9b8fd940993df4691b987e65a124b21b751
Guilherme Moreira <ceschiatti@gmail.com>
2023-10-03 10:39:15 -0300

    Add a ChadCn button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
     return (
         <main className="">
             <h1> Main</h1>
+            <Button>
+                Button
+            </Button>
         </main>
     )
 }
